443a830edaee04bbef76d9f257af752a66a295bc
Kiran Namawar <kirannamawar@gmail.com>
2025-03-22 15:08:23 +0530

    Add suspense fallback for loading state in AuthLayout and update postinstall script in package.json

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Suspense } from 'react';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 
     return (
         <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ''}>
             <div className="flex flex-col items-center justify-center h-screen">
-                {children}
+                <Suspense fallback={<div className='loading loading-ring loading-xl text-yellow-400'></div>}> 
+                    {children}
+                </Suspense>
             </div>
         </GoogleOAuthProvider>
     );
